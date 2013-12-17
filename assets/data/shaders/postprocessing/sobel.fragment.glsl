5e0d3b02b0eb71154990f890d9e08a9f5fe77840
infinity8 <lyeeedar@gmail.com>
2013-12-17 18:51:54 +0000

    Better heightmap added

--- assets/data/shaders/postprocessing/sobel.fragment.glsl
+++ assets/data/shaders/postprocessing/sobel.fragment.glsl
@@ -5,42 +5,45 @@
 varying vec4 v_color;
 varying vec2 v_texCoords;
 uniform sampler2D u_texture;
-uniform float limit;
+uniform float u_threshold;
+uniform float width;
+uniform float height;
+uniform vec3 darken;
+uniform float alpha_offset;
 
 const vec3 W = vec3(0.2125, 0.7154, 0.0721);
 
-float intensity(in vec4 color)
+float intensity(vec4 color)
 {
-    return color.xyz * W;//sqrt((color.x*color.x)+(color.y*color.y)+(color.z*color.z));
+    return color.xyz * W;
 }
  
-float sobel(float offset, vec2 center)
+vec4 sobel(float offsetx, float offsety, vec2 center)
 {
     // get samples around pixel
-    float tleft = intensity(texture2D(u_texture,center + vec2(-offset,offset)));
-    float left = intensity(texture2D(u_texture,center + vec2(-offset,0)));
-    float bleft = intensity(texture2D(u_texture,center + vec2(-offset,-offset)));
-    float top = intensity(texture2D(u_texture,center + vec2(0,offset)));
-    float bottom = intensity(texture2D(u_texture,center + vec2(0,-offset)));
-    float tright = intensity(texture2D(u_texture,center + vec2(offset,offset)));
-    float right = intensity(texture2D(u_texture,center + vec2(offset,0)));
-    float bright = intensity(texture2D(u_texture,center + vec2(offset,-offset)));
+    vec3 tleft =    texture2D(u_texture,center + vec2(-offsetx,offsety)).rgb;
+    vec3 left =     texture2D(u_texture,center + vec2(-offsetx,0)).rgb;
+    vec3 bleft =    texture2D(u_texture,center + vec2(-offsetx,-offsety)).rgb;
+    vec3 top =      texture2D(u_texture,center + vec2(0,offsety)).rgb;
+    vec3 bottom =   texture2D(u_texture,center + vec2(0,-offsety)).rgb;
+    vec3 tright =   texture2D(u_texture,center + vec2(offsetx,offsety)).rgb;
+    vec3 right =    texture2D(u_texture,center + vec2(offsetx,0)).rgb;
+    vec3 bright =   texture2D(u_texture,center + vec2(offsetx,-offsety)).rgb;
  
     // Sobel masks (to estimate gradient)
     //        1 0 -1     -1 -2 -1
     //    X = 2 0 -2  Y = 0  0  0
     //        1 0 -1      1  2  1
  
-    float x = tleft + 2.0*left + bleft - tright - 2.0*right - bright;
-    float y = -tleft - 2.0*top - tright + bleft + 2.0 * bottom + bright;
-    float color = sqrt((x*x) + (y*y));
-    if (color > 0.1){return 0.0;}
-    return 1.0;
+    vec3 x = tleft + 2.0*left + bleft - tright - 2.0*right - bright;
+    vec3 y = -tleft - 2.0*top - tright + bleft + 2.0 * bottom + bright;
+    vec3 colour = x-y;
+    float color = length(colour);//sqrt((x*x) + (y*y));
+    if (color > u_threshold){return vec4(0.0, 0.0, 0.0, color+alpha_offset);}
+    return vec4(0.0,0.0,0.0,0.0);
  }
  
 void main(void)
 {
-    float offset = 1.0/1000.0;
-    gl_FragColor.rgb = sobel(offset, v_texCoords);
-    gl_FragColor.a = 1.0;
+    gl_FragColor = sobel(1.0/width, 1.0/height, v_texCoords);
 }
