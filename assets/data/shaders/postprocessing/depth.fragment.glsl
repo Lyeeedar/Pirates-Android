f4bbbf3aab09055a4d9f4a6afd0e8245b97d3271
infinity8 <lyeeedar@gmail.com>
2014-01-08 17:27:12 +0000

    Depth of Field effect. Add discard to modelbatcher to allow for post processing effects using depth buffer to work.

--- assets/data/shaders/postprocessing/depth.fragment.glsl
+++ assets/data/shaders/postprocessing/depth.fragment.glsl
@@ -4,15 +4,13 @@
 
 varying vec4 v_color;
 varying vec2 v_texCoords;
+uniform sampler2D u_depth;
 uniform sampler2D u_texture;
 uniform float u_far;
 
 void main() {
-
 	vec4 colour = texture2D(u_texture, v_texCoords);
-
-	float z = ( colour.r * 2.0 ) - 1.0;
-
-    gl_FragColor.rgb = z;
-    gl_FragColor.a = 1.0;
+	float z = 4.0 / (u_far + 2.0 - texture2D(u_depth, v_texCoords).x * (u_far - 2.0));
+    gl_FragColor.rgb = colour.rgb;
+    gl_FragColor.a = z;
 }
